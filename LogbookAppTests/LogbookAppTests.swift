960abb4d365975cf355bc400128a3291986616fe
Mathew Bellamy <mat.bellamy@icloud.com>
2025-02-15 20:52:34 +0000

    Initial Commit

---
+++ LogbookAppTests/LogbookAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  LogbookAppTests.swift
+//  LogbookAppTests
+//
+//  Created by Mathew Bellamy on 15/02/2025.
+//
+
+import Testing
+@testable import LogbookApp
+
+struct LogbookAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ LogbookAppUITests/LogbookAppUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  LogbookAppUITestsLaunchTests.swift
+//  LogbookAppUITests
+//
+//  Created by Mathew Bellamy on 15/02/2025.
+//
+
+import XCTest
+
+final class LogbookAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

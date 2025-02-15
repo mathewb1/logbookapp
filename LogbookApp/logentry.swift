bfb5c50d332e30261279a9656f1d1def48c0996b
Mathew Bellamy <mat.bellamy@icloud.com>
2025-02-15 21:30:53 +0000

    test changes

---
+++ LogbookApp/logentry.swift
@@ -0,0 +1,15 @@
+struct LogEntry {
+    let date: Date
+    let title: String
+    let description: String
+    
+    func getFormattedDate() -> String {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .medium
+        return formatter.string(from: date)
+    }
+    
+    func getSummary() -> String {
+        return "[\(getFormattedDate())] \(title): \(description)"
+    }
+}
